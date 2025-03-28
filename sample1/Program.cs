7e39df9aa7c48f70d18638f9defb431292b2e5ea
Lisa Lupi <lisa.lupi@student-cs.fr>
2025-03-28 13:05:55 +0000

    premiere partie

---
+++ sample1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace sample1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person("John", 30);
+        person.Hello(true);
+        person.Hello(false);
+    }
+}
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public void Hello(bool isLowercase)
+    {
+        string output = $"Hello {Name}, your are {Age}";
+        if (isLowercase)
+        {
+            Console.WriteLine(output.ToLower());
+        }
+        else
+        {
+            Console.WriteLine(output.ToUpper());
+        }
+    }
+}
